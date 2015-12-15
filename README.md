b6ede4e92a8ef3d674ea85b1654a7b3fb3688ebd
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-12-15 13:57:43 +0900

    Initial import.
    Generated by stagehand.

---
+++ README.md
@@ -0,0 +1,19 @@
+# render_tests
+
+A library for Dart developers. It is awesome.
+
+## Usage
+
+A simple usage example:
+
+    import 'package:render_tests/render_tests.dart';
+
+    main() {
+      var awesome = new Awesome();
+    }
+
+## Features and bugs
+
+Please file feature requests and bugs at the [issue tracker][tracker].
+
+[tracker]: http://example.com/issues/replaceme
---
+++ lib/render_tests.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// The render_tests library.
+///
+/// This is an awesome library. More dartdocs go here.
+library render_tests;
+
+// TODO: Export any libraries intended for clients of this package.
+
+export 'src/render_tests_base.dart';
